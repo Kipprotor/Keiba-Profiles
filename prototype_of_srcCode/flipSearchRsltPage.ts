b15c4a7c843d4d1d33540c1b7eabebc38810476b
Kipprotor <124500516+Kipprotor@users.noreply.github.com>
2024-02-28 01:39:00 +0000

    Rename: "debug lab" folder to "prototype_of_srcCode". This is because it more correctly represents the purpose for which the contents were created.

---
+++ prototype_of_srcCode/flipSearchRsltPage.ts
@@ -0,0 +1,28 @@
+import { searchOnNetkeiba,scrapeSearchResult } from "../src/index.ts";
+ import { searchResult } from "../src/model.ts";
+
+let result: searchResult[] = [];
+
+let i = 1
+while (true) {
+  const res = await searchOnNetkeiba({fatherName:"ゴールドシップ", page:i})
+  console.log(`url: ${res.url}\nuniqueness: ${res.unique}\n`);
+
+  const rows = scrapeSearchResult(res.html)
+  if (rows.length > 0) {
+    result = result.concat(rows);
+    //console.log({rows})
+    i++;
+    await sleep(2000);
+  } else {
+    break
+  }  
+}
+
+console.log(`result length: ${result.length}\n`,"Search result:\n", {result})
+
+
+function sleep(ms: number): Promise<void>{
+  // msミリ秒後に解決されるPromiseを返す
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
--- prototype_of_srcCode/raceResultParser.ts
+++ prototype_of_srcCode/raceResultParser.ts
@@ -3,6 +3,9 @@ import { cheerio } from "../src/deps.ts";
 
 const res = await searchOnNetkeiba({ horseName: "パンサラッサ" });
 const $ = cheerio.load(res.html);
-const sel = 'table[class="db_h_race_results nk_tb_common"] > tbody';
+const sel = 'table[class="db_h_race_results nk_tb_common"] > tbody > tr';
 const resultTable = $(sel);
-console.log(resultTable);
+resultTable.each((_,element) => {
+  const td = $(element).find("td").text().replaceAll("\n","");
+  console.log(td)
+})
